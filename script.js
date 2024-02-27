298cccb83d0b042ac40b8cea776e99498d10b55a
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-02-28 00:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+let selected = "circle-1";
+// click event for next btn
+nextBtn.addEventListener("click", (e) => {
+  if (selected) {
+    const currentCircle = selected;
+    const circElement = document.getElementById(currentCircle);
+    const lineElement = circElement.nextElementSibling;
+    const nextCircle = lineElement.nextElementSibling;
+    if (nextCircle) {
+      lineElement.classList.toggle("active");
+      nextCircle.classList.toggle("active");
+      selected = nextCircle.id;
+    }
+  }
+});
+
+// click event for prev btn
+prevBtn.addEventListener("click", (e) => {
+  const currentCircle = selected;
+  const circElement = document.getElementById(currentCircle);
+  const lineElement = circElement.previousElementSibling;
+  const prevCircle = lineElement.previousElementSibling;
+  if (prevCircle) {
+    lineElement.classList.toggle("active");
+    selected = prevCircle.id;
+
+    circElement.classList.toggle("active");
+  }
+});
